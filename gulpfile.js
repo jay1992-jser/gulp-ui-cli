a81ff799b7c529127c1a54d42975446a60a13ca7
谢培 <Xp123456>
2021-06-08 15:39:25 +0800

    完善打包逻辑

--- gulpfile.js
+++ gulpfile.js
@@ -9,212 +9,165 @@ var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var comments = require('postcss-discard-comments');
 var rename = require('gulp-rename');
-var sourcemaps = require('gulp-sourcemaps');
 var browserSync = require('browser-sync');
-var childProcess = require('child_process');
 var pkg = require('./package.json');
 var convertCssVar = require('gulp-convert-css-var');
-var exec = require('child_process').exec;
+var del = require('del');
+const { src, dest, series, parallel, watch } = gulp
+const bs = browserSync.create()
 
 var yargs = require('yargs').options({
-    w: {
-        alias: 'watch',
-        type: 'boolean',
-    },
-    s: {
-        alias: 'server',
-        type: 'boolean',
-    },
-    p: {
-        alias: 'port',
-        type: 'number',
-    },
+  w: {
+      alias: 'watch',
+      type: 'boolean',
+  },
+  s: {
+      alias: 'server',
+      type: 'boolean',
+  },
+  p: {
+      alias: 'port',
+      type: 'number',
+  },
 }).argv;
 
-var option = { base: 'src' };
-var dist = __dirname + '/dist';
+var option = { 
+  base: 'src',
+  dist: __dirname + '/dist'
+};
 
-function exec(cmd) {
-    return new Promise((resolve, reject) => {
-        const process = childProcess.exec(cmd, (error) => {
-            if (error) {
-                reject(error);
-            } else {
-                resolve();
-            }
-        });
-        process.stdout.on('data', function(data) {
-            console.log(data);
-        });
-    });
+const clean = () => {
+  return del(['dist', 'temp'])
 }
 
-gulp.task('build:style', function() {
-    var banner = [
-        '/*!',
-        ' * WeUI v<%= pkg.version %> (<%= pkg.homepage %>)',
-        ' * Copyright <%= new Date().getFullYear() %> Tencent, Inc.',
-        ' * Licensed under the <%= pkg.license %> license',
-        ' */',
-        '',
-    ].join('\n');
-    gulp
-    .src('src/style/weui.less', option)
-    .pipe(sourcemaps.init())
+
+const build_style = () => {
+  var banner = [
+    '/*!',
+    ' * v<%= pkg.version %> (<%= pkg.homepage %>)',
+    ' * Copyright <%= new Date().getFullYear() %>',
+    ' * Licensed under the <%= pkg.license %> license',
+    ' */',
+    '',
+  ].join('\n');
+  return src('src/style/weui.less', { base: option.base })
     .pipe(
-        less().on('error', function(e) {
-            console.error(e.message);
-            this.emit('end');
-        }),
+      less().on('error', function(e) {
+        console.error(e.message);
+        this.emit('end');
+      }),
     )
     .pipe(postcss([autoprefixer(['iOS >= 7', 'Android >= 4.1']), comments()]))
     .pipe(convertCssVar())
     .pipe(header(banner, { pkg: pkg }))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(dist))
+    .pipe(dest(option.dist))
     .pipe(browserSync.reload({ stream: true }))
     .pipe(
-        nano({
-            zindex: false,
-            autoprefixer: false,
-            svgo: false,
-        }),
+      nano({
+        zindex: false,
+        autoprefixer: false,
+        svgo: false,
+      }),
     )
-    .pipe(
-        rename(function(path) {
-            path.basename += '.min';
-        }),
-    )
-    .pipe(gulp.dest(dist));
-});
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(dest(option.dist));
+}
 
-gulp.task('build:example:assets', function() {
-    gulp
-    .src('src/example/**/*.?(png|jpg|gif|js)', option)
-    .pipe(gulp.dest(dist))
-    .pipe(browserSync.reload({ stream: true }));
-});
+const build_example_assets = () => {
+  return  src('src/example/**/*.?(png|jpg|gif|js)', { base: option.base })
+    .pipe(dest(option.dist))
+    .pipe(bs.reload({ stream: true }));
+}
 
-gulp.task('build:example:style', function() {
-    gulp
-    .src('src/example/example.less', option)
-    .pipe(
-        less().on('error', function(e) {
-            console.error(e.message);
-            this.emit('end');
-        }),
-    )
-    .pipe(postcss([autoprefixer(['iOS >= 7', 'Android >= 4.1'])]))
-    .pipe(
-        nano({
-            zindex: false,
-            autoprefixer: false,
-        }),
-    )
-    .pipe(gulp.dest(dist))
-    .pipe(browserSync.reload({ stream: true }));
-});
+const build_example_style = () => {
+  return src('src/example/**/*.less', { base: option.base })
+  .pipe(
+    less().on('error', function(e) {
+      this.emit('end');
+    })
+  )
+  .pipe(postcss([autoprefixer(['iOS >= 7', 'Android >= 4.1'])]))
+  .pipe(
+    nano({
+      zindex: false,
+      autoprefixer: false,
+    })
+  )
+  .pipe(dest(option.dist))
+  .pipe(bs.reload({ stream: true }));
+}
 
-gulp.task('build:example:html', function() {
-    gulp
-    .src('src/example/index.html', option)
-    .pipe(
-        tap(function(file) {
-            var dir = path.dirname(file.path);
-            var contents = file.contents.toString();
-            contents = contents.replace(
-                /<link\s+rel="import"\s+href="(.*)">/gi,
-                function(match, $1) {
-                    var filename = path.join(dir, $1);
-                    var id = path.basename(filename, '.html');
-                    var content = fs.readFileSync(filename, 'utf-8');
-                    return (
-                        '<script type="text/html" id="tpl_' +
-                        id +
-                        '">\n' +
-                        content +
-                        '\n</script>'
-                    );
-                },
-            );
-            file.contents = new Buffer(contents);
-        }),
-    )
-    .pipe(gulp.dest(dist))
-    .pipe(browserSync.reload({ stream: true }));
-});
-
-gulp.task('build:example', [
-    'build:example:assets',
-    'build:example:style',
-    'build:example:html',
-]);
-
-gulp.task('build', ['build:style', 'build:example']);
-
-gulp.task('watch', ['build'], function() {
-    var list = [
-        {
-            path: 'src/style/**/*',
-            task: ['build:style'],
+const build_example_html = () => {
+  return src('src/example/index.html', { base: option.base })
+  .pipe(
+    tap(function(file) {
+      var dir = path.dirname(file.path);
+      var contents = file.contents.toString();
+      contents = contents.replace(
+        /<link\s+rel="import"\s+href="(.*)">/gi,
+        function(match, $1) {
+          var filename = path.join(dir, $1);
+          var id = path.basename(filename, '.html');
+          var content = fs.readFileSync(filename, 'utf-8');
+          return (
+            '<script type="text/html" id="tpl_' +
+            id +
+            '">\n' +
+            content +
+            '\n</script>'
+          );
         },
-        {
-            path: 'src/example/example.less',
-            task: ['build:example:style'],
-        },
-        {
-            path: 'src/example/**/*.?(png|jpg|gif|js)',
-            task: ['build:example:assets'],
-        },
-        {
-            path: 'src/**/*.html',
-            task: ['build:example:html'],
-        },
-    ];
-    list.forEach((item) => {
-        var timeout = null;
-        gulp.watch(path.resolve(__dirname, item.path)).on('change', function() {
-            clearTimeout(timeout);
+      );
+      file.contents = Buffer.from(contents);
+    }),
+  )
+  .pipe(dest(option.dist))
+  .pipe(bs.reload({ stream: true }));
+}
 
-            timeout = setTimeout(() => {
-                gulp.run(item.task);
-            }, 300);
-        });
-    });
-});
+const serve = () => {
+  watch('src/style/**/*', build_style)
+  watch('src/example/example.less', build_example_style)
+  watch('src/example/**/*.?(png|jpg|gif|js)', build_example_assets)
+  watch('src/**/*.html', build_example_html)
 
-gulp.task('server', function() {
-    yargs.p = yargs.p || 8080;
-    browserSync.init({
-        server: {
-            baseDir: './dist',
-        },
-        ui: {
-            port: yargs.p + 1,
-            weinre: {
-                port: yargs.p + 2,
-            },
-        },
-        port: yargs.p,
-        startPath: '/example',
-    });
-});
+  watch([
+    'src/example/assets/images/**',
+    'src/example/assets/fonts/**'
+  ], bs.reload)
 
-gulp.task('tag', function() {
-    const tag = `v${pkg.version}`;
-    exec(`git tag ${tag}`);
-});
+  yargs.p = yargs.p || 8080;
 
-// 参数说明
-//  -w: 实时监听
-//  -s: 启动服务器
-//  -p: 服务器启动端口，默认8080
-gulp.task('default', ['build'], function() {
-    if (yargs.s) {
-        gulp.start('server');
+  bs.init({
+    notify: true,
+    port: yargs.p,
+    // open: false,
+    // files: 'dist/**',
+    ui: {
+      port: yargs.p + 1,
+      weinre: {
+          port: yargs.p + 2,
+      },
+    },
+    server: {
+      baseDir: './dist'
+      // baseDir: ['temp', 'src', 'public'],
+      // routes: {
+      //   '/node_modules': 'node_modules'
+      // }
     }
+  })
+}
 
-    if (yargs.w) {
-        gulp.start('watch');
-    }
-});
+const build_example = parallel(build_example_assets, build_example_style, build_example_html)
+
+const build = series(clean, build_style, build_example)
+
+const develop = series(build, serve)
+
+module.exports = {
+  ['build:style']: build_style,
+  ['build:example']: build_example,
+  build,
+  develop
+}
